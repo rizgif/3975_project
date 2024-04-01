82f45ca44bf3d24c6dd13d0a347425769f6237ca
Davin Leong <davinleongg@gmail.com>
2024-04-01 10:55:26 -0700

    Added login authentication and admin user

--- backend_laravel_breeze/database/seeders/DatabaseSeeder.php
+++ backend_laravel_breeze/database/seeders/DatabaseSeeder.php
@@ -5,19 +5,20 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\AdminUserSeeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
+  /**
+   * Seed the application's database.
+   */
+  public function run(): void
+  {
+    // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+    $this->call([
+      AdminUserSeeder::class,
+
+    ]);
+  }
 }
