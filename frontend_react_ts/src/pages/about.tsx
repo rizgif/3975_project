9b2008e3bd4ff48261c5ad236f63a684a64c71bc
“Riz” <rnursaidy@gmail.com>
2024-04-08 15:54:06 -0700

    npm install.

---
+++ frontend_react_ts/src/pages/about.tsx
@@ -0,0 +1,26 @@
+// About.tsx
+
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>
+        Welcome to our React app! This is the About page where you can learn more about us.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sagittis justo id lectus finibus,
+        nec vulputate sem ultricies. Nullam pulvinar nisi nec ultricies viverra. In condimentum tellus
+        ut tincidunt feugiat.
+      </p>
+      <p>
+        Sed luctus ultrices leo, a faucibus nulla consectetur ac. Nam vehicula elit at dolor commodo,
+        vel lobortis arcu aliquet. Vestibulum ut tincidunt enim. Duis vel pretium nisi. In hac habitasse
+        platea dictumst.
+      </p>
+    </div>
+  );
+};
+
+export default About;
